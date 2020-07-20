2480c6c251d84cecaac42353f0a79eb44ce1e330
Chuck Stein <cfgstein@gmail.com>
2020-07-19 22:51:32 -0400

    use dictionary to store pulledBodies instead of list

--- Assets/Scripts/BlackHolePull.cs
+++ Assets/Scripts/BlackHolePull.cs
@@ -6,19 +6,17 @@ public class BlackHolePull : MonoBehaviour
     
     public float gravitationalForce = 1000f;
     
-    private List<Rigidbody> pulledBodies;
-    private SphereCollider pullCollider;
+    private Dictionary<int, Rigidbody> pulledBodies; // key is the ID of rigidbody's gameobject
     
     private void Start()
     {
-        pulledBodies = new List<Rigidbody>();
-        pullCollider = GetComponent<SphereCollider>();
+        pulledBodies = new Dictionary<int, Rigidbody>();
     }
     
     private void OnTriggerEnter(Collider other)
     {
         var pulledBody = other.gameObject.GetComponent<Rigidbody>();
-        if (pulledBody) pulledBodies.Add(pulledBody);
+        if (pulledBody) pulledBodies.Add(other.gameObject.GetInstanceID(), pulledBody);
     }
 
     private void OnTriggerExit(Collider other)
@@ -29,7 +27,7 @@ public class BlackHolePull : MonoBehaviour
 
     private void FixedUpdate()
     {
-        foreach (var pulledBody in pulledBodies)
+        foreach (var pulledBody in pulledBodies.Values)
         {
             var singularityPosition = transform.position;
             var pulledBodyPosition = pulledBody.transform.position;
@@ -41,7 +39,8 @@ public class BlackHolePull : MonoBehaviour
 
     public void RemovePulledBody(Rigidbody pulledBody)
     {
-        if (pulledBody && pulledBodies.Contains(pulledBody)) pulledBodies.Remove(pulledBody);
+        var pulledBodyId = pulledBody.gameObject.GetInstanceID();
+        if (pulledBody && pulledBodies.ContainsKey(pulledBodyId)) pulledBodies.Remove(pulledBodyId);
     }
     
 }
