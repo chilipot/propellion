a94cee36bf7671820a794a836f460ba83498b383
Unix-Code <dmalakh@comcast.net>
2020-08-05 23:53:33 -0400

    Remove dmg debug msg

--- Assets/Scripts/AsteroidCollisionHealthEffect.cs
+++ Assets/Scripts/AsteroidCollisionHealthEffect.cs
@@ -2,15 +2,11 @@
 
 public class AsteroidCollisionHealthEffect : HealthEffectBehavior
 {
-    public override int ComputeStrength(Collision collision = null)
-    {
-        var dmg = collision == null
-            ? 0
-            : Mathf.RoundToInt(Mathf.Clamp(collision.impulse.magnitude / Time.fixedDeltaTime * (damageMultiplier / 1000), 1f,
-                maxDmg));
-        Debug.Log(dmg);
-        return dmg;
-    }
+    public override int ComputeStrength(Collision collision = null) => collision == null
+        ? 0
+        : Mathf.RoundToInt(Mathf.Clamp(collision.impulse.magnitude / Time.fixedDeltaTime * (damageMultiplier / 1000),
+            1f,
+            maxDmg));
 
 public override HealthEffect Effect => HealthEffect.Damage;
 
