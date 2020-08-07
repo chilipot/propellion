e11913ba843c6188e6d7fc2072859a66121648d4
Unix-Code <dmalakh@comcast.net>
2020-08-07 00:55:39 -0400

    Add basic main menu scene

---
+++ Assets/Scripts/MenuBehavior.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuBehavior : MonoBehaviour
+{
+    // THIS IS A PLACEHOLDER BEFORE WE HAVE A LEVEL SELECT
+    // TODO: REPLACE THIS WITH A PROPER LEVEL SELECT
+    
+    public string firstLevel;
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(firstLevel);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
