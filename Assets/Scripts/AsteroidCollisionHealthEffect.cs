9789f04ef43c9e02f5497c1fed5a2ec6a790df60
Unix-Code <dmalakh@comcast.net>
2020-08-07 01:26:16 -0400

    Re-fix asteroid collision health effect

--- Assets/Scripts/AsteroidCollisionHealthEffect.cs
+++ Assets/Scripts/AsteroidCollisionHealthEffect.cs
@@ -4,13 +4,12 @@ public class AsteroidCollisionHealthEffect : HealthEffectBehavior
 {
     public override int ComputeStrength(Collision collision = null) => collision == null
         ? 0
-        : Mathf.RoundToInt(Mathf.Clamp(collision.impulse.magnitude / Time.fixedDeltaTime * (damageMultiplier / 1000),
-            1f,
-            maxDmg));
+        : Mathf.RoundToInt(Mathf.Clamp(collision.impulse.magnitude / Time.fixedDeltaTime / 1000 * damageMultiplier,
+            1f, maxDmg));
 
-public override HealthEffect Effect => HealthEffect.Damage;
+    public override HealthEffect Effect => HealthEffect.Damage;
 
-    public float damageMultiplier = 1f;
+    public float damageMultiplier = 1.5f;
     public float maxDmg = 30f;
 
 }
