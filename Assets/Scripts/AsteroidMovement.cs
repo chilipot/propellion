9e1f46b44cec4b4769a99bf20a97bb6fae72d80a
chuck-stein <cfgstein@gmail.com>
2021-02-06 13:45:45 -0500

    Add occasional speedy asteroids

--- Assets/Scripts/AsteroidMovement.cs
+++ Assets/Scripts/AsteroidMovement.cs
@@ -5,6 +5,8 @@ public class AsteroidMovement : MonoBehaviour
 {
 
     public Vector2 speedRange;
+    public float superspeedChance;
+    public float superspeedMultiplier;
     
     private float speed;
     private Rigidbody rb;
@@ -12,6 +14,11 @@ public class AsteroidMovement : MonoBehaviour
     private void Start()
     {
         speed = Random.Range(speedRange[0], speedRange[1]);
+        var superspeed = Random.Range(0F, 1F) <= superspeedChance;
+        if (superspeed)
+        {
+            speed *= superspeedMultiplier;
+        }
         rb = GetComponent<Rigidbody>();
         var tf = transform;
         rb.AddForce(tf.forward * speed, ForceMode.Impulse);
